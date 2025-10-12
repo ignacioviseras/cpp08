692a47853b5df1ea91c8919d35ff610f1348fe16
iviseras <ignacio.viseras@gmail.com>
2025-10-12 02:54:34 +0200

    ex02 ok?

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,3 +1,58 @@
+#pragma once
+#include <stack>
+#include <deque>
+#include <iostream>
+
+//std::deque its a container, it use fifo interface 
+// and it can access the 2 extremes of the container
+
+// std::stack its a adapter of containers only 
+// u can pop push and access but only in the top node
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		typedef typename Container::iterator iterator;
+		typedef typename Container::const_iterator const_iterator;
+		typedef typename Container::reverse_iterator reverse_iterator;
+		typedef typename Container::const_reverse_iterator const_reverse_iterator;
+		MutantStack(): std::stack<T, Container>() {}
+		MutantStack(const MutantStack &other): std::stack<T, Container>(other) {}
+		~MutantStack() {}
+
+		MutantStack &operator=(const MutantStack &other) {
+			if (this != &other) {
+				std::stack<T, Container>::operator=(other);
+			}
+			return (*this);
+		}
+		
+		iterator begin() {
+			return (this->c.begin());
+		}
+		const_iterator begin() const{
+			return (this->c.begin());
+		}
+		reverse_iterator rbegin() {
+			return (this->c.rbegin());
+		}
+		const_reverse_iterator rbegin() const{
+			return (this->c.rbegin());
+		}
+
+		iterator end() {
+			return (this->c.end());
+		}
+		const_iterator end() const{
+			return (this->c.end());
+		}
+		reverse_iterator rend() {
+			return (this->c.rend());
+		}
+		const_reverse_iterator rend() const{
+			return (this->c.rend());
+		}
+};
 
 
 // Now, it’s time to move on to more serious things. Let’s develop something weird.
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,25 +1,47 @@
-int main()
-{
+#include "MutantStack.hpp"
+
+int main() {
+	//test subject
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
+	std::cout << "top: ";
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
+	std::cout << "size: ";
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	//reverse iteration
+	std::cout << "Reverse iteration" << std::endl;
+	for (MutantStack<int>::reverse_iterator revIt = mstack.rbegin(); revIt != mstack.rend(); ++revIt) {
+		std::cout << *revIt << std::endl;
+	}
+	std::cout << std::endl;
+	
+	std::cout << "use string" << std::endl;
+	MutantStack<std::string> strStack;
+	strStack.push("paco");
+	strStack.push("pepe");
+	strStack.push("juan");
+	for (MutantStack<std::string>::iterator it = strStack.begin(); it != strStack.end(); ++it)
+		std::cout << *it << std::endl;
+
 	return 0;
 }
